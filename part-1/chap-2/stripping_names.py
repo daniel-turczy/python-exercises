8aafdd5b0bf131c9382a128621aeb1811682ea5a
Dan <danturczyniak@gmail.com>
2025-07-29 14:21:10 +0100

    Update stripping_names.py
    added docstring with task from the book

--- part-1/chap-2/stripping_names.py
+++ part-1/chap-2/stripping_names.py
@@ -1,4 +1,11 @@
-name = "    Christian   "
+"""
+Use a variable to represent a person's name, and include some whitespace
+characters at the beginning and end of the name. Make sure you use each
+character combination, "\t" and "\n", at least once. Then print the name using
+each of the three stripping functions, lstrip(), rstrip(), and strip().
+"""
+
+name = "\n   Christian  \t\t   "
 print("Before:")
 print(name)
 
