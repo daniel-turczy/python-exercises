f68dabf8a945e37c66c39e2425cf212291ce3a5a
Dan <danturczyniak@gmail.com>
2025-07-29 14:32:18 +0100

    upload chapter 2 files

---
+++ part-1/chapter-2/famous_quote_2.py
@@ -0,0 +1,9 @@
+"""
+Represent a famous person's name using a variable called famous_person.
+Then compose your message and represent it with a new variable called message.
+Print your message.
+"""
+
+famous_person = "Edward Snowden"
+quote = '"I would rather be without a state than without a voice"'
+print(f"{quote} - {famous_person}")
